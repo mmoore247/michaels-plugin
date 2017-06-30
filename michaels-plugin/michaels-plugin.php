82fd0f9cd819d3e8996aa62771babd878b83f3e9
mmoore247 <topekamike247@gmail.com>
2017-06-30 15:49:39 -0500

    formatting changeublic_html/trunk/wp-content/plugins/michaels-plugi

--- michaels-plugin/michaels-plugin.php
+++ michaels-plugin/michaels-plugin.php
@@ -1,6 +1,4 @@
 <?php
-
-
 /*
 ugin Name: Michael's Plugin
 Plugin URI: https://justbloat.com/michaels-plugin
@@ -14,14 +12,14 @@ License: GPLv2
 function mm_mp_display_notice() {
 	$user_id = get_current_user_id();
 	if ( !get_user_meta( $user_id, 'mm_mp_dismiss_notice' ) ) {
-  	echo '<div class="mm_mp_dismiss_notice">' . _e( 'Thanks for installing my plugin that will do nothing!', 'michaels-plugin' ) . '<a href="?mm_mp_dismiss_notice">Dismiss</a></div>';
+	echo '<div class="mm_mp_dismiss_notice">' . _e( 'Thanks for installing my plugin that will do nothing!', 'michaels-plugin' ) . __('<a href="?mm_mp_dismiss_notice">Dismiss</a></div>');
 		}
 }
 add_action( 'admin_notices', 'mm_mp_display_notice' );
 function mm_mp_dismiss_notice() {
 	$user_id = get_current_user_id();
 	if ( isset( $_GET['mm_mp_dismiss_notice'] ) ) {
-		add_user_meta( $user_id, 'mm_mp_dismiss_notice', 'true', false );
+		add_user_meta( $user_id, 'mm_mp_dismiss_notice', 'true', true );
 		}
 }
 add_action( 'admin_init', 'mm_mp_dismiss_notice' );
