47811a97423acf30be946f652818fadb1f2bee46
mmoore247 <topekamike247@gmail.com>
2017-07-02 18:36:32 -0500

    uninstall hook

--- michaels-plugin.php
+++ michaels-plugin.php
@@ -1,11 +1,12 @@
 <?php
+
 /*
-ugin Name: Michael's Plugin
+Plugin Name: Michael's Plugin
 Plugin URI: https://justbloat.com/michaels-plugin
 Description: This plugin will not add any value to your site. When activated it will display a message on its usefulness.
 Author: Michael Moore
 Author URI: https://justbloat.com
-Version: 1.1
+Version: 1.2
 Text Domain: michaels-plugin
 License: GPLv2
 */
@@ -23,3 +24,10 @@ function mm_mp_dismiss_notice() {
 		}
 }
 add_action( 'admin_init', 'mm_mp_dismiss_notice' );
+register_uninstall_hook( __FILE__, 'plugin_uninstall' );
+function plugin_uninstall () {
+	$all_user_ids = get_users( 'fields=ID' );
+	foreach ( $all_user_ids as $user_id ) {
+    delete_user_meta( $user_id, 'mm_michaels_notice_dismissed' );
+}
+}
